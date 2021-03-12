a59899af53b3dc1b281e564e042ee0f667b7fe7c
Marlon Menegatti <marlonmenegatt@gmail.com>
2021-03-12 18:48:22 -0300

    test: add test to list_legth

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,8 +1,15 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "return list legth" do
+      list = [5, 4, 3, 2, 1]
+
+      response = ListLength.call(list)
+
+      expected_response = 5
+
+      assert expected_response == response
+    end
   end
 end
